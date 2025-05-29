b912bd46ba7b88f506f3cee6b5fcd902df455754
Elena Shilova <e.shilova@vniias.ru>
2025-05-29 08:39:49 +0300

    update_docker

--- ewm-service/src/main/java/ru/practicum/EwmServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/EwmServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwnServiceApp {
+public class EwmServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(EwnServiceApp.class, args);
+        SpringApplication.run(EwmServiceApp.class, args);
     }
 }
